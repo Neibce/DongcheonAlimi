8eea21c376ed242ef62ca46e0ca7996db612dde1
Yang Jun-Young <rex711@naver.com>
2020-07-03 00:14:33 +0900

    post 메서드 추가

--- app/src/main/java/dev/jun0/dcalimi/util/RequestHttpURLConnection.java
+++ app/src/main/java/dev/jun0/dcalimi/util/RequestHttpURLConnection.java
@@ -13,11 +13,40 @@ public class RequestHttpURLConnection {
     public String get(String _url) throws IOException, NetworkErrorException {
         return get(_url, 15000);
     }
+
+    public String post(String _url) throws IOException, NetworkErrorException {
+        return get(_url, 15000);
+    }
+
     public String get(String _url, int timeoutMills) throws IOException, NetworkErrorException {
         try {
             URL url = new URL(_url);
             HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
             urlConn.setRequestMethod("GET");
+            urlConn.setConnectTimeout(timeoutMills);
+
+            if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK)
+                throw new NetworkErrorException();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), StandardCharsets.UTF_8));
+            String strLine;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((strLine = reader.readLine()) != null) {
+                stringBuilder.append(strLine);
+            }
+            return stringBuilder.toString();
+        } catch (IOException | NetworkErrorException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public String post(String _url, int timeoutMills) throws IOException, NetworkErrorException {
+        try {
+            URL url = new URL(_url);
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            urlConn.setRequestMethod("POST");
             urlConn.setConnectTimeout(timeoutMills);
 
             if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK)
